a97c5aba67dd3c7c03e79558190e6242a185b0b3
shimon <shimonababa@gmail.com>
2025-04-18 15:47:05 +0300

    -

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { Menu, X, Phone, Fan, HomeIcon, Info, Package2, Settings, MapPin } from 'lucide-react'
+import { Menu, X, Phone, Fan, HomeIcon, Info, Package2, MapPin } from 'lucide-react'
 import { Button } from '../ui/button'
 
 const Navbar = () => {
